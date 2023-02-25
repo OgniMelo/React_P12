0e6df23f82a627f1678a7653514e8d5355e8d29c
Melody <86132511+OgniMelo@users.noreply.github.com>
2023-02-25 08:47:04 +0100

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,11 @@ import Navbar from './components/Navbar'
 import Menu from './components/Menu'
 import Profile from './pages/Profile'
 
+/**
+@function
+@returns {JSX.Element} - A JSX element containing the Navbar, Menu and Profile components.
+*/
+
 export default () => {
 	return (
 		<>
